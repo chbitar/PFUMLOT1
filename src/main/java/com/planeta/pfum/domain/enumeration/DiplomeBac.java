14257eb03fc37a89d6a54bd507f6756e7f2ec45c
Garar Jamal <garar.jamal@gmail.com>
2020-11-23 02:23:31 +0100

    liste type de bac

--- src/main/java/com/planeta/pfum/domain/enumeration/DiplomeBac.java
+++ src/main/java/com/planeta/pfum/domain/enumeration/DiplomeBac.java
@@ -4,5 +4,9 @@ package com.planeta.pfum.domain.enumeration;
  * The DiplomeBac enumeration.
  */
 public enum DiplomeBac {
-    Sciences_De_La_Vie_Et_De_La_Terre, Sciences_Physiques_Et_Chimiques, Sciences_Economiques, Techniques_De_Gestion_Et_Comptabilite
-}
+	Sciences_De_La_Vie_Et_De_La_Terre, Sciences_Physiques_Et_Chimiques, Sciences_Maths_A, Sciences_Maths_B,
+	Sciences_Economiques, Techniques_De_Gestion_Et_Comptabilite, Sciences_agronomiques,
+	Sciences_Et_Technologies_Electriques, Sciences_Et_Technologies_Mecaniques, Arts_Appliques, Sciences_Humaines,
+	Sciences_de_la_Chariaa, Sciences_de_Langue_Arabe, Autre
+}	
+
